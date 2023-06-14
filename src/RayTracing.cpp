af1576ce9d0ff62e9a1a6dd8d50fae8c9a4334cb
engbergandreas <adde.engberg@gmail.com>
2023-06-14 01:33:37 -0400

    Setup OpenGL with GLFW & Glad
    added GLFW and GLAD to setup openGL. GLFW should be setup automatically through CMAKE and compiled together with the application.

--- src/RayTracing.cpp
+++ src/RayTracing.cpp
@@ -8,10 +8,11 @@
 #include <chrono>
 #include "Utils.h"
 #include <thread>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
 
-int main()
-{
-	//std::cout << nThreads << " concurrent threads are supported\n";
+void raytracingCPU() {
 	Scene scene{};
 	std::string filename{ settings::FILENAME };
 
@@ -27,5 +28,94 @@ int main()
 	std::cout << "Finished rendering in: " << duration.count() << " seconds\n";
 
 	camera.writeToFile(filename);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void cursor_pos_callback(GLFWwindow* window, double xpos, double ypos);
+
+void processInput(GLFWwindow* window);
+
+GLFWwindow* initGLFW() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(
+		settings::CAMERA_PLANE_WIDTH, settings::CAMERA_PLANE_HEIGHT,
+		"RayTracer", NULL, NULL);
+
+	if (window == NULL)
+		return NULL;
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, cursor_pos_callback);
+	return window;
+}
+
+/// <summary>
+/// Check whether OpenGL was setup correctly, returns false if there was a failure
+/// </summary>
+/// <param name="window">window context to validate</param>
+bool validateOpenGL(GLFWwindow* window) {
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return false;
+	}
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return false;
+	}
+	return true;
+}
+
+int main()
+{
+	GLFWwindow* window{ initGLFW() };
+	if (!validateOpenGL(window))
+		return -1;
+
+	//Setup initial size of window
+	glViewport(0, 0, settings::CAMERA_PLANE_WIDTH, settings::CAMERA_PLANE_HEIGHT);
+
+
+	while (!glfwWindowShouldClose(window))
+	{
+		//Input
+		processInput(window);
+
+		//Render
+		glClearColor(0.4f, 0.1f, 0.9f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
+}
+
+void processInput(GLFWwindow* window)
+{
+	//Close window on escape key
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+	std::cout << "width: " << width << " height: " << height << '\n';
+}
+
+void cursor_pos_callback(GLFWwindow* window, double xpos, double ypos) {
+	std::cout << "cursor move to screen pos x: " << xpos << " y: " << ypos << '\n';
 }
